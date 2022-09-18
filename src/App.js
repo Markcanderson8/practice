7a2371d20b4c422bf43cb72fc77afa3d3ec7a965
Mark Anderson <markcanderson8@gmail.com>
2022-09-18 11:41:26 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
-import List from "./List";
 
 function App() {
-  const items = [
-    {
-      title: "React",
-      url: "https://react.js.org/",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
-
   return (
     <div className='App'>
       <h1>My Hacker Stories</h1>
-
-      <label htmlFor='search'>Search: </label>
-      <input id='search' type='text' />
-
-      <hr />
     </div>
   );
 }
